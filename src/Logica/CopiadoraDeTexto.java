a90cc6038fdaba359a89d24eab1b4275660e5187
roma <rbrian.0111@gmail.com>
2025-11-19 12:47:50 -0300

    Primer commit
    Es donde se suben los primeros archivos del proyectos.

---
+++ src/Logica/CopiadoraDeTexto.java
@@ -0,0 +1,14 @@
+package Logica;
+import IGU.Pantalla;
+
+public class CopiadoraDeTexto {
+
+    public static void main(String[] args) {
+        //Nos conectamos a la pantalla
+        Pantalla pant = new Pantalla();
+        pant.setVisible(true); //Hacemos visible
+        pant.setLocationRelativeTo(null); //Ubicamos en la posición en el medio
+        
+    }
+
+}
